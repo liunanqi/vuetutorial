d814d827049562cdefb5ac41ffc7052f3e79667a
liunanqi <liunanqizero@outlook.com>
2021-03-22 15:26:41 +0800

    add pics

--- vueinstance.js
+++ vueinstance.js
@@ -4,3 +4,16 @@ var app = new Vue({
       message: 'Hello Vue!'
     }
 })
+
+var data = {
+    newTodoText: '',
+    visitCount: 0,
+    hideCompletedTodos: false,
+    todos: [],
+    error: null
+  }
+
+var vm = new Vue({
+    //options
+    data : data
+})
