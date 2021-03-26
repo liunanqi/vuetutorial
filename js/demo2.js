c5ff409cb365a0bc2dd6b551da5ad057e578dfb7
liunanqi <liunanqizero@outlook.com>
2021-03-26 11:38:51 +0800

    add watch demo

--- js/demo2.js
+++ js/demo2.js
@@ -14,3 +14,32 @@ Vue.createApp({
         }
     }
 }).mount("#app")
+
+const watchExample = Vue.createApp({
+    data() {
+        return {
+            question:'',
+            answer:'Questions usually contain a question mark. ;-)'
+        }
+    },
+    watch: {
+        question(newQ, oldQ) {
+            if(newQ.indexOf('?') > -1) {
+                this.getAnswer()
+            }
+        }
+    },
+    methods: {
+        getAnswer() {
+            this.answer = 'Thinking...'
+            axios
+            .get('https://yesno.wtf/api')
+            .then(response => {
+                this.answer = response.data.answer
+            })
+            .catch(error => {
+                this.answer = 'Error! Could not reach the API. ' + error
+            })
+        }
+    }
+}).mount('#watch-example')
