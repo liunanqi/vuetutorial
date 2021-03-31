33ea0a58cb60cbb26ab56e78bbae17b32d457c1b
liunanqi <liunanqizero@outlook.com>
2021-03-31 16:35:31 +0800

    hello files

--- js/axiosdemo.js
+++ js/axiosdemo.js
@@ -1,17 +1,15 @@
 Vue.createApp({
-  el: '#app',
-  data () {
-    return {
-      info: null
+    data () {
+      return {
+        info: null
+      }
+    },
+    mounted () {
+      axios
+        .get('https://www.runoob.com/try/ajax/json_demo.json')
+        .then(response => (this.info = response))
+        .catch(function (error) { // 请求失败处理
+          console.log(error);
+        });
     }
-  },
-  mounted () {
-    axios
-      .get('https://www.runoob.com/try/ajax/json_demo.json')
-      .then(response => (this.info = response))
-      .catch(function (error) { // 请求失败处理
-        console.log('error...\n')
-        console.log(error);
-      });
-  }
-})
+  }).mount("#app")
--- js/hellovue.js
+++ js/hellovue.js
@@ -1,65 +1,71 @@
-var app = new Vue({
-    el: '#app',
-    data: {
-      message: 'Hello Vue!'
+var app = Vue.createApp({
+    data() {
+      return{
+        message: 'Hello Vue!'
+      }
     }
-})
+}).mount("#app")
   
-var app2 = new Vue({
-  el: '#app-2',
-  data: {
-    info: '页面加载于 ' + new Date().toLocaleString()
+var app2 = Vue.createApp({
+  data() {
+    return{
+      info: '页面加载于 ' + new Date().toLocaleString()
+    }
   }
-})
+}).mount("#app-2")
 
-var app3 = new Vue({
-  el: '#app-3',
-  data: {
-    seen: true
+var app3 = Vue.createApp({
+  data(){
+    return {
+      seen: true
+    }
   }
-})
+}).mount("#app-3")
 
-var app4 = new Vue({
-  el: '#app-4',
-  data: {
-    todos: [
-      { text: '学习 JavaScript' },
-      { text: '学习 Vue' },
-      { text: '整个大项目' }
-    ]
+var app4 = Vue.createApp({
+  data() {
+    return {
+      todos: [
+        { text: '学习 JavaScript' },
+        { text: '学习 Vue' },
+        { text: '整个大项目' }
+      ]
+    }
   }
-})
+}).mount("#app-4")
 
-var app5 = new Vue({
-  el: '#app-5',
-  data: {
+var app5 = Vue.createApp({
+  data(){
+    return{
       message: 'Hello Vue,js'
+    }
   },
   methods: {
-    reverseMessage:function() {
+    reverseMessage() {
         this.message = this.message.split('').reverse().join('')
     }
   }
-})
+}).mount("#app-5")
 
-var app6 = new Vue({
-    el:'#app-6',
-    data: {
-        message:'Hello model S!!!'
+var app6 = Vue.createApp({
+    data() {
+      return{
+        message:'model S'
+      }
     }
-})
+}).mount("#app-6")
 
-Vue.component('book-item',{
-    props:['action', 'bookname'],
-    template:'<li>{{ action + \' the \' + bookname }}</li>'
-})
-var app7 = new Vue({
-    el:'#app-7',
-    data:{
-        bookList:[
+var app7 = Vue.createApp({
+    data(){
+        return{
+          bookList:[
             {id:1, text:'Harry Potter', action:'burned'},
             {id:2, text:'Ghost in the Shell', action:'finished'},
             {id:3, text:'Sherlock Holmes', action:'lost'}
-        ]
+          ]
+        }
     }
-})
+}).component('book-item',{
+  props:['action', 'bookname'],
+  template:'<li>{{ action + \' the \' + bookname }}</li>'
+}).mount("#app-7")
