8b1a0106b4360cbe199f5a13d71e4b4dd17c34c0
liunanqi <liunanqizero@outlook.com>
2021-03-31 17:16:18 +0800

    add packages

--- js/axiosdemo.js
+++ js/axiosdemo.js
@@ -1,15 +1,17 @@
 Vue.createApp({
-    data () {
-      return {
-        info: null
-      }
-    },
-    mounted () {
-      axios
-        .get('https://www.runoob.com/try/ajax/json_demo.json')
-        .then(response => (this.info = response))
-        .catch(function (error) { // 请求失败处理
-          console.log(error);
-        });
+  data () {
+    return {
+      info: null
     }
-  }).mount("#app")
+  },
+  mounted () {
+    console.log('axios start...\n')
+    axios
+      .get('https://www.runoob.com/try/ajax/json_demo.json')
+      .then(response => (this.info = response.data.sites))
+      .catch(function (error) { // 请求失败处理
+        console.log('error...\n')
+        console.log(error);
+      });
+  }
+}).mount("#app")
